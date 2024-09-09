d83fc31c33f7275fab0b3f78b808d9e15761d43e
Josephine Bedon <bedonjosephine01@gmail.com>
2024-09-09 14:42:01 +0800

    second commit

--- routes/web.php
+++ routes/web.php
@@ -1,18 +1,34 @@
 <?php
-
+use App\Models\Student;
 use Illuminate\Support\Facades\Route;
 
-/*
-|--------------------------------------------------------------------------
-| Web Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider and all of them will
-| be assigned to the "web" middleware group. Make something great!
-|
-*/
-
-Route::get('/', function () {
-    return view('welcome');
+Route::get('/students/create', function () {
+    $student = new Student();
+    $student->first_name = "John";
+    $student->last_name = "Doe";
+    $student->email = "johndoe@example.com";
+    $student->age = 22;
+    $student->save();
+    return 'Student created!';
 });
+
+Route::get('/students', function () {
+    $students = Student::all();
+    return $students;
+});
+
+Route::get('/students/update', function () {
+    $student = Student::where('email', 'johndoe@example.com')->first();
+    $student->email = "john.doe@newmail.com";
+    $student->age = 23; // Update age as well
+    $student->save();
+    return 'Student updated!';
+});
+
+Route::get('/students/delete', function () {
+    $student = Student::where('email', 'john.doe@newmail.com')->first();
+    $student->delete();
+    return 'Student deleted!';
+});
+
+?>
